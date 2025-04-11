2eb4d71b3b41aca5910d14570915248896f4ed75
seshu000 <panduseshagiri675@gmail.com>
2025-04-11 20:18:03 +0530

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,4 +1,10 @@
-input_data=[1,2,[3,4,[6,7],6],5,[8,9,0]]
+#Q-1
+# Flattens the list 
+#ie input = [1,2,3, [1,2,3,[3,4],2]]
+# output = [1,2,3,1,2,3,3,4,2]
+
+
+input_data=[1,2,3, [1,2,3,[3,4],2]]
 
 def flatten(lst1):
     output=[]
@@ -10,6 +16,24 @@ def flatten(lst1):
     return output
 print(flatten(input_data))   
 
+
+#Q-2
+#input = [[[ '(0,1,2)' , '(3,4,5)'], ['(5,6,7)' , '(9,4,2)']]]
+#output = [[[[0,1,2],[3,4,5]],[[5,6,7],[9,4,2]]]]
+
+in_data = [[[ '(0,1,2)' , '(3,4,5)'], ['(5,6,7)' , '(9,4,2)']]]
+
+def convert(data):
+    result = []
+    for item in data:
+        if isinstance(item, list):
+            result.append(convert(item))
+        else:
+            numbers = list(map(int, item.strip("()").split(",")))
+            result.append(numbers)
+    return result
+
+print(convert(in_data))
 
 
 
