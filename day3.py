25b44e4faf2a37ed466d9dd3dbf9e5bdf3c1cd78
seshu000 <panduseshagiri675@gmail.com>
2025-04-11 07:18:43 +0530

    Update day3.py

--- day3.py
+++ day3.py
@@ -11,14 +11,5 @@ def flatten(lst1):
 print(flatten(input_data))   
 
 
-def process_nested_structure(in_data):
-    output=[]
-    for i in in_data:    
-        if isinstance(i,tuple):
-            o=list(map(int, i.strip("()").split(",")))
-            output.append(o)
-    return output
-in_data = [[[ '(0,1,2)' , '(3,4,5)'], ['(5,6,7)' , '(9,4,2)']]]
-print(process_nested_structure(in_data))
 
 
