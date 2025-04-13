34d22f8b91945bfa14bcc4d7a647bf70629b7ce1
seshu000 <panduseshagiri675@gmail.com>
2025-04-13 17:31:24 +0530

    Update day6.py

--- day6.py
+++ day6.py
@@ -1,3 +1,10 @@
+#question9
+'''Given a URL, download that and parse 
+and download all links inside that page 
+    in asyncio 
+    BeautifulSoup for parsing html, requests for downloading'''
+
+
 import asyncio
 import aiohttp
 from bs4 import BeautifulSoup
