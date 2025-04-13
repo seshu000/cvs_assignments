14716e209477c6eb3e51153b2d965a8e5fb7cf7c
seshu000 <panduseshagiri675@gmail.com>
2025-04-13 13:38:10 +0530

    Update day4_1.py

--- day4_1.py
+++ day4_1.py
@@ -1,6 +1,25 @@
-from pkg_poly import Poly  
+from itertools import zip_longest
 
+class Poly:
+    def __init__(self, *coef):
+        self.coef = list(coef)
+        
+    def __str__(self):
+        coef_str = ', '.join(str(c) for c in self.coef)
+        return "Poly(" + coef_str + ")"
+
+    def __add__(self, other):
+        result_coef = [
+            a + b for a, b in zip_longest(self.coef[::-1], other.coef[::-1], fillvalue=0)
+        ][::-1]
+        return Poly(*list(result_coef))
+
+
+
+
+from pkg_poly import Poly  
 a = Poly(1, 2, 3) 
 b = Poly(1, 0, 1, 1, 2, 3)  
 c = a + b
 print(c) 
+
