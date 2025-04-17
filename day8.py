043499c762e04bbaf02bba125c1a79cb62a433e3
seshu000 <panduseshagiri675@gmail.com>
2025-04-17 11:54:26 +0530

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, Response, jsonify
+import random
+
+weather_app = Flask(__name__)
+
+@weather_app.route('/w/<location>', methods=['GET'])
+def w(location):
+    fmt = request.args.get('format', 'json').lower()
+    degree = f"{random.randint(35, 45)}°C"
+    
+    if fmt == 'xml':
+        xml_data = f'''
+        <forecast>
+            <location>{location}</location>
+            <temp>{degree}</temp>
+            <scale>celsius</scale>
+        </forecast>
+        '''
+        return Response(xml_data.strip(), content_type='application/xml')
+   
+    return jsonify({
+        "location": location,
+        "temp": degree,
+        "scale": "celsius"
+    })
+
+if __name__ == '__main__':
+    weather_app.run(debug=True)
